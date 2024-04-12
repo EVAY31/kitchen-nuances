35dc3f16d00cf5efbc0ee8a07c0643826ae7c9bb
Selena555 <ko4kareva.elena@yandex.ru>
2024-04-12 20:25:09 +0300

    clear dev

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,12 +16,3 @@ use App\Http\Controllers\CategoryController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('categories', 'CategoryController@index');
-//Route::get('/basket/index', 'BasketController@index')->name('basket.index');
-//Route::get('/basket/checkout', 'BasketController@checkout')->name('basket.checkout');
-
-Route::post('/basket/add/{product}', 'BasketController@add')->name('basket.add');
-Route::resource('categories', CategoryController::class)->only(['index', 'show']);
-Route::post('/basket/plus/{product}', 'BasketController@plus')->name('basket.plus');
-Route::post('/basket/minus/{product}', 'BasketController@minus')->name('basket.minus');
